fb10892714a6399d2d4ffa867e3a0b646a4c164d
BHAUTIK04 <sens.bhautik@gmail.com>
2017-02-09 00:08:37 +0530

    html file added

--- API_Doc/settings.py
+++ API_Doc/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'API_Doc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR+'/create_API/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
